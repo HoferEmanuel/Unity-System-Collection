65d31112b554b5365d29b26ebb1f3dc0eaf41503
Emanuel Hofer <102549117+HoferEmanuel@users.noreply.github.com>
2022-10-17 12:40:21 +0200

    new171022w

--- UIManager/UIWindow.cs
+++ UIManager/UIWindow.cs
@@ -14,35 +14,42 @@ public class UIWindow : MonoBehaviour
         UIWindowManager.current.OnOpenWindow += OpenWindow;
         UIWindowManager.current.OnCloseWindow += CloseWindow;
         UIWindowManager.current.OnOpenSingleWindow += OpenSingleWindow;
-        UIWindowManager.current.OnCloseAllWindows += CloseAllWindows;
+        UIWindowManager.current.OnCloseAllWindows += CloseWindow;
     }
 
     public void OpenWindow(int targetID)
     {
+        if (id == targetID)
+            OpenWindow();
 
     }
+
+    public void OpenWindow() => menuWindow.SetActive(true);
 
     public void CloseWindow(int targetID)
     {
-
+        if (id == targetID)
+            CloseWindow();
     }
+
+    public void CloseWindow() => menuWindow.SetActive(false);
 
     public void OpenSingleWindow(int targetID)
     {
-
+        if (id == targetID)
+            OpenWindow();
+        else
+            CloseWindow();
     }
     
-    public void CloseAllWindows()
-    {
-
-    }
-
     private void OnDestroy()
     {
         
         UIWindowManager.current.OnOpenWindow -= OpenWindow;
         UIWindowManager.current.OnCloseWindow -= CloseWindow;
         UIWindowManager.current.OnOpenSingleWindow -= OpenSingleWindow;
-        UIWindowManager.current.OnCloseAllWindows -= CloseAllWindows;   
+        UIWindowManager.current.OnCloseAllWindows -= CloseAllWindows;  
+
+        UIWindowManager.current.RemoveUIWindow(); 
     }
 }
