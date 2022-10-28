40928935fdf06ac9a62be52f18f30f06821379f1
Emanuel Hofer <102549117+HoferEmanuel@users.noreply.github.com>
2022-10-28 09:19:32 +0200

    Update InputManagerTest.cs

--- InputSystemManager/InputManagerTest.cs
+++ InputSystemManager/InputManagerTest.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class InputManagerTest : MonoBehaviour
 {
+    public static InputManagerTest current;
+
+    private void Awake()
+    {
+        current = this;
+    }
+
     public void PauseMenu() => Debug.Log("pausing menu");
 
     public void ToggleInventory => Debug.Log("toggleInventory");
 
-    public void PlayerJump()
-    {
-
-    }
+    public void PlayerJump() => Debug.Log("PlayerJump");
 
     private void PlayerMove()
     {
-
+        
     }
 }
